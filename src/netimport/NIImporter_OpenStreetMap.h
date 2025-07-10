60114bb7ed11b628d1a625bc77b7e78a93f5b9a6
Marcel Pi <169088301+MarcelPiNacy-CVC@users.noreply.github.com>
2025-07-10 12:52:48 +0200

    Remove enum duplicate

--- src/netimport/NIImporter_OpenStreetMap.h
+++ src/netimport/NIImporter_OpenStreetMap.h
@@ -70,17 +70,6 @@ public:
 
 protected:
 
-    /** @enum CycleWayType
-     * @brief details on the kind of cycleway along this road
-     */
-    enum WayType {
-        WAY_NONE = 0,
-        WAY_FORWARD = 1,
-        WAY_BACKWARD = 2,
-        WAY_BOTH = WAY_FORWARD | WAY_BACKWARD,
-        WAY_UNKNOWN = 4
-    };
-
     /** @brief An internal representation of an OSM-node
      */
     struct NIOSMNode : public Parameterised {
